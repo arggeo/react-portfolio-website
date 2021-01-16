234d6f654c24fb947e5fad3b5e4635f797e58d81
argygeor <signup@arggeo.com>
2021-01-16 21:52:46 +0200

    Navigation

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import GlobalStyle from './components/GlobalStyle';
 // Import Pages
 import AboutUs from './pages/AboutUs';
 
+// Import Components
+import Nav from './components/Nav';
+
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
+            <Nav />
             <AboutUs />
         </div>
     );
