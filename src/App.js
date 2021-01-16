e15331b15d42796aabd8a6e42efc54d9e50042d9
argygeor <signup@arggeo.com>
2021-01-16 22:20:22 +0200

    Router & additional pages
    Added router for navigation and sample pages: Our Work & Contact Us

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import GlobalStyle from './components/GlobalStyle';
 
 // Import Pages
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 
 // Import Components
 import Nav from './components/Nav';
+
+// Import Router
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
             <Nav />
-            <AboutUs />
+            <Switch>
+                <Route path="/" exact>
+                    <AboutUs />
+                </Route>
+                <Route path="/work">
+                    <OurWork />
+                </Route>
+                <Route path="/contact">
+                    <ContactUs />
+                </Route>
+            </Switch>
         </div>
     );
 }
