241cadb9f1b2368a24ebc051eb77236dca4ffe24
argygeor <signup@arggeo.com>
2021-01-16 10:47:05 +0200

    FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions? <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How do I start?</h4>
+                <div className="answer">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum dolor nulla. Curabitur lectus purus.
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily schedule</h4>
+                <div className="answer">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum dolor nulla. Curabitur lectus purus.
+                </div>
+            </div>
+            <div className="question">
+                <h4>Payment methods</h4>
+                <div className="answer">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum dolor nulla. Curabitur lectus purus.
+                </div>
+            </div>
+            <div className="question">
+                <h4>What services do you offer?</h4>
+                <div className="answer">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum dolor nulla. Curabitur lectus purus.
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FaqSection;
